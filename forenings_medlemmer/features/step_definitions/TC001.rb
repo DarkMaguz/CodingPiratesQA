4f7c5cc71525c1bfce592c4de7bfef33ae52003e
Peter Balling <peter@knowmio.com>
2017-01-21 00:27:37 +0100

    Implimented TC001

--- forenings_medlemmer/features/step_definitions/TC001.rb
+++ forenings_medlemmer/features/step_definitions/TC001.rb
@@ -1,13 +1,29 @@
+require 'selenium-cucumber'
+
 Given(/^I fillout the form$/) do
 	navigate_to($new_user_url)
 	wait(1)
 	select_option_from_dropdown(:id, :text, "Dreng", $new_user_gender_select_id)
-	enter_text(:id, "Oskar", $new_user_gender_child_name_id)
-	wait(5)
+	enter_text(:id, "Oskar", $new_user_child_name_id)
+  enter_text(:id, "01-04-2004", $new_user_child_birthday_id)
+  click(:id,"id_child_email")
+  enter_text(:id, "oskar7@sfkshkfs.com", "id_child_email")
+  enter_text(:id, "12345678", "id_child_phone")
+  enter_text(:id, "Ole", "id_parent_name")
+  enter_text(:id, "ole7@sfkshkfs.com", "id_parent_email")
+  enter_text(:id, "32165487", "id_parent_phone")
+  enter_text(:id, "Øster Voldgade 4A, 1350 København K", "search-address")
+  wait(1)
+  click(:id,"ui-id-2")
+  wait(1)
 end
 
 When(/^I submit the form$/) do
+  click(:xpath, "//input[@value='Opret']")
+  wait(1)
 end
 
 Then(/^I should see something$/) do
+  expect(is_element_displayed(:xpath, "/html/body/div/div/div/div/h1")).to be true
+  expect(get_element_text(:xpath, "/html/body/div/div/div/div/h1").to_s == "Email er på vej!").to be true
 end
--- forenings_medlemmer/features/support/new_user_object_repository.rb
+++ forenings_medlemmer/features/support/new_user_object_repository.rb
@@ -6,4 +6,12 @@ $new_user_header_xpath = "/html/body/div[1]/div[1]/div[1]/h1"
 
 # TC001
 $new_user_gender_select_id = "id_child_gender"
-$new_user_gender_child_name_id = "id_child_name"
+$new_user_child_name_id = "id_child_name"
+$new_user_child_birthday_id = "id_child_birthday"
+$new_user_child_email_id = "id_child_birthday"
+$new_user_child_phone_id = "id_child_birthday"
+$new_user_parent_name_id = "id_child_birthday"
+$new_user_parent_email_id = "id_child_birthday"
+$new_user_parent_phone_id = "id_child_birthday"
+$new_user_search_address_id = "search-address"
+$new_user_address_search_result_id = "ui-id-2"
