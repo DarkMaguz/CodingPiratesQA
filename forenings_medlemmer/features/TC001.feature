c1bee67b4320c39d0a3d328f95bc1377f8535c0a
Peter Balling <peter@knowmio.com>
2017-01-21 16:26:29 +0100

    Refined TC001

--- forenings_medlemmer/features/TC001.feature
+++ forenings_medlemmer/features/TC001.feature
@@ -2,6 +2,7 @@ Feature: Add a new user
 	As a parent, I want to be able to register my child, so that my child gets on the waiting list.
 
 Scenario: I want to register a child
-	Given I fillout the form
-	When I submit the form
+	Given I am on the signup page
+	When I fillout the form
+	And I submit the form
 	Then I should see something
--- forenings_medlemmer/features/step_definitions/TC001.rb
+++ forenings_medlemmer/features/step_definitions/TC001.rb
@@ -1,8 +1,10 @@
-require 'selenium-cucumber'
-
-Given(/^I fillout the form$/) do
+Given(/^I am on the signup page$/) do
 	navigate_to($new_user_url)
 	wait(1)
+  expect(is_element_displayed(:xpath, $new_user_header_xpath)).to be true
+end
+
+When(/^I fillout the form$/) do
 	select_option_from_dropdown(:id, :text, "Dreng", $new_user_gender_select_id)
 	enter_text(:id, "Oskar", $new_user_child_name_id)
   enter_text(:id, "01-04-2004", $new_user_child_birthday_id)
@@ -18,12 +20,12 @@ Given(/^I fillout the form$/) do
   wait(1)
 end
 
-When(/^I submit the form$/) do
+And(/^I submit the form$/) do
   click(:xpath, "//input[@value='Opret']")
   wait(1)
 end
 
-Then(/^I should see something$/) do
+Then(/^I should see a message telleing me that an email has been sent$/) do
   expect(is_element_displayed(:xpath, "/html/body/div/div/div/div/h1")).to be true
   expect(get_element_text(:xpath, "/html/body/div/div/div/div/h1").to_s == "Email er på vej!").to be true
 end
